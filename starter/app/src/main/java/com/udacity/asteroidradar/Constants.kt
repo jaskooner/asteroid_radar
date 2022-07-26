e01eeb01c0635e76609a649d758f6ea18714d21a
jaskooner <jaskooner.singh@gmail.com>
2022-07-26 17:21:42 +0100

    added near earth asteroid path

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -5,4 +5,6 @@ object Constants {
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
     const val API_KEY = "uPe3xx1BRwPVHzfIIW3tJibWluhox9NE67zjVkfw"
+    const val NEO_PATH = "/neo/rest/v1/feed?"
+
 }
