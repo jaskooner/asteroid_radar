36250ebb60ae7d2777688b02ed3c4af9a1dfe082
jaskooner <jaskooner.singh@gmail.com>
2022-08-03 16:35:07 +0100

    added API key hint
    advised th ereader where to put their nasa API in

--- starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
+++ starter/app/src/main/java/com/udacity/asteroidradar/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val API_QUERY_DATE_FORMAT = "YYYY-MM-dd"
     const val DEFAULT_END_DATE_DAYS = 7
     const val BASE_URL = "https://api.nasa.gov/"
-    const val API_KEY = "uPe3xx1BRwPVHzfIIW3tJibWluhox9NE67zjVkfw"
+    const val API_KEY = "DEMO"
     const val NEO_PATH = "neo/rest/v1/feed?"
     const val IMG_DAY = "planetary/apod?"
 
